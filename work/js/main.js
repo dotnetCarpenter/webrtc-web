945c427f7a02baf9a1d04852e46350f459aa53ee
dotnetCarpenter <jon.ronnenberg@gmail.com>
2018-02-10 18:24:01 +0100

    finished first step: https://codelabs.developers.google.com/codelabs/webrtc-web/#3

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,49 @@
-'use strict';
+'use strict'
 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+
+const contraints = {
+  audio: false,
+  video: true
+}
+
+const video = document.querySelector('video')
+
+function successCallback (stream) {
+  window.stream = stream
+
+  try {
+    video.srcObject = stream
+  } catch (error) {
+    console.error(error)
+    if (window.URL) {
+      video.src = window.URL.createObjectURL(stream)
+    } else {
+      video.src = stream
+    }
+  }
+
+  // if (window.URL) {
+  //   video.src = window.URL.createObjectURL(stream)
+  // } else {
+  //   video.src = stream
+  // }
+}
+
+function errorCallback (error) {
+  console.error('navigator.getUserMedia error: ', error)
+}
+
+navigator.getUserMedia(contraints, successCallback, errorCallback)
+
+document.querySelectorAll('button').forEach(btn => {
+  btn.addEventListener('click', event => {
+    const name = btn.classList.item(0)
+    addFilter(name)
+  }, false)
+})
+
+function addFilter (name) {
+  video.classList.remove(video.classList.item(0))
+  video.classList.add(name)
+}
